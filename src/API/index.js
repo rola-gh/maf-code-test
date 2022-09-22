988c52a82e63b8af633561dc2456bbc3a6c0513f
rola-gh <rola8355@gmail.com>
2022-09-22 10:39:48 +0300

    handle pagination and get list per pagination

--- src/API/index.js
+++ src/API/index.js
@@ -4,6 +4,6 @@ import axios from "axios";
 export const getImageList =  (per_page, page ) =>{
    return  axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API_KEY}&tags=technology&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1`)
         .then((res)=>{
-            return res.data.photos?.photo
+            return res.data.photos
         })
 }
--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import './App.css';
 import {useEffect, useState} from "react";
-import {Col, Row} from 'antd';
+import {Col, Pagination, Row} from 'antd';
 import {getImageList} from "./API";
 
 function App() {
     const [pics, setPics] = useState([])
     const [perPage, setPerPage] = useState(10)
     const [page, setPage] = useState(1)
+    const [total, setTotal] = useState(1)
 
     /**Get Item list **/
     useEffect( () => {
-         getImageList(perPage, page).then(res => setPics(res))
+         getImageList(perPage, page).then(res => {
+             setPics(res.photo)
+             setTotal(res.pages)
+         } )
     },[perPage, page])
+
+    /**Pagination**/
+    const onChangePagination = (pageNumber,pageSize) => {
+        setPage(pageNumber)
+        setPerPage(pageSize)
+    };
 
   return (
     <div className="App">
         <div className='wrapper'>
             <Row justify="start" gutter={10}>
             {pics?.map(pic=>
-                <Col xs={24} xl={8} >
+                <Col xs={24} xl={8}>
                     <div className='image_wrapper'>
-                        <img alt="flickr" src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`}/>
+                        <img alt="flickr"
+                             src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`}
+                        />
                     </div>
                 </Col>
             )}
             </Row>
+            <div className='pagination'>
+                <Pagination  defaultCurrent={1}
+                             total={total}
+                             onChange={onChangePagination}
+                             pageSizeOptions={[10, 20, 50]}
+                />
+            </div>
         </div>
     </div>
   );
