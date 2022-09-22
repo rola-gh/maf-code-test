2bb29ee431dd8ef15980847ac70a690c84772ca8
rola-gh <rola8355@gmail.com>
2022-09-22 10:19:31 +0300

    Get photo list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import {Col, Row} from 'antd';
+import {getImageList} from "./API";
 
 function App() {
+    const [pics, setPics] = useState([])
+    const [perPage, setPerPage] = useState(10)
+    const [page, setPage] = useState(1)
+
+    /**Get Item list **/
+    useEffect( () => {
+         getImageList(perPage, page).then(res => setPics(res))
+    },[perPage, page])
+
   return (
     <div className="App">
-      <h1>MAF CODE TEST</h1>
+        <div className='wrapper'>
+            <Row justify="start" gutter={10}>
+            {pics?.map(pic=>
+                <Col xs={24} xl={8} >
+                    <div className='image_wrapper'>
+                        <img alt="flickr" src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`}/>
+                    </div>
+                </Col>
+            )}
+            </Row>
+        </div>
     </div>
   );
 }
