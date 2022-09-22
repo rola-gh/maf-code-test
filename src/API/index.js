0dde22bb4119b3ee8743c82e81323eb10a4c9f25
rola-gh <rola8355@gmail.com>
2022-09-22 13:14:58 +0300

    add search by item

--- src/API/index.js
+++ src/API/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import axios from "axios";
 
-export const getImageList =  (per_page, page ) =>{
-   return  axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API_KEY}&tags=technology&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1`)
+export const getImageList =  (per_page, page , text_search_val) =>{
+   return  axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.REACT_APP_API_KEY}&tags=technology&per_page=${per_page}&page=${page}&format=json&nojsoncallback=1&text=${text_search_val}`)
         .then((res)=>{
             return res.data.photos
         })
--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import './App.css';
 import {useEffect, useState} from "react";
 import {Col, Pagination, Row} from 'antd';
 import {getImageList} from "./API";
+import { Input } from 'antd';
+const { Search } = Input;
 
 function App() {
     const [pics, setPics] = useState([])
     const [perPage, setPerPage] = useState(10)
     const [page, setPage] = useState(1)
     const [total, setTotal] = useState(1)
+    const [searchValue, setSearchValue] = useState('')
 
     /**Get Item list **/
     useEffect( () => {
-         getImageList(perPage, page).then(res => {
+         getImageList(perPage, page, searchValue).then(res => {
              setPics(res.photo)
              setTotal(res.pages)
          } )
-    },[perPage, page])
+    },[perPage, page, searchValue])
 
     /**Pagination**/
     const onChangePagination = (pageNumber,pageSize) => {
@@ -23,19 +26,32 @@ function App() {
         setPerPage(pageSize)
     };
 
-  return (
-    <div className="App">
+    /**Search by item**/
+    const onSearch = (value) => {
+        setSearchValue(value)
+    }
+
+    return (
+      <div className="App">
+        <div className='search-wrapper'>
+            <Search
+                addonBefore="Search by"
+                placeholder="Search item"
+                allowClear
+                onSearch={onSearch}
+            />
+        </div>
         <div className='wrapper'>
             <Row justify="start" gutter={10}>
-            {pics?.map(pic=>
-                <Col xs={24} xl={8}>
-                    <div className='image_wrapper'>
-                        <img alt="flickr"
-                             src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`}
-                        />
-                    </div>
-                </Col>
-            )}
+                {pics?.map(pic=>
+                    <Col xs={24} xl={8}>
+                        <div className='image_wrapper'>
+                            <img alt="flickr"
+                                 src={`https://farm${pic.farm}.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}.jpg`}
+                            />
+                        </div>
+                    </Col>
+                )}
             </Row>
             <div className='pagination'>
                 <Pagination  defaultCurrent={1}
@@ -46,7 +62,7 @@ function App() {
             </div>
         </div>
     </div>
-  );
+    );
 }
 
 export default App;
